edf71f73b3baf3a5e4c70f31baf7f80240716e69
Kolton Pham <kolton.phamous@gmail.com>
2025-01-16 17:22:24 -0600

    add program.cs

---
+++ Wk1Ex1/Wk1Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk1Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ITSE1302");
+            Console.ReadLine();
+        }
+    }
+}
