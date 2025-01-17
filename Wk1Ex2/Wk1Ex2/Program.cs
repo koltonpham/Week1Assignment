cc4e1bb9d705d2a0e5c4d66bdeab963ea500c90b
Kolton Pham <kolton.phamous@gmail.com>
2025-01-16 18:24:24 -0600

    second program

---
+++ Wk1Ex2/Wk1Ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk1Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?"); //writing to the console the questions to be asked
+            string name = Console.ReadLine();              //saving the input from the user as variable
+            Console.WriteLine("What is your last name?");  
+            string lName = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            //printing out the full sentence using all the varibles that was entered in by the user
+            Console.WriteLine("Hello, " + name + " " + lName + "! You are " + age + " years old and your favorite color is " + color + ".");
+            
+        }
+    }
+}
